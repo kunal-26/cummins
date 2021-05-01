0801f09629d12197b7b81c09bbe3341fe444c61b
Kunal Jain <kunajain2608@gmail.com>
2021-05-01 16:00:32 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+jQuery(document).ready(function($) {
+
+    'use strict';
+
+
+    $('.counter').each(function() {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+
+        $({ countNum: $this.text() }).animate({
+                countNum: countTo
+            },
+
+            {
+
+                duration: 8000,
+                easing: 'linear',
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $this.text(this.countNum);
+                    //alert('finished');
+                }
+
+            });
+
+    });
+
+
+
+
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > 100) {
+            $(".header").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+            $(".header").removeClass("active");
+        }
+    });
+
+
+
+});
